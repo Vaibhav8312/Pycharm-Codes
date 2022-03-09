1055bfed86c5d5faf1f87e52731a849cdc67d473
Vaibhav <vaibhavkhullar83@gmail.com>
2022-03-09 18:53:16 +0530

    Basic modification.

--- 4. Intermediate+/birthday-wisher-normal-start/main.py
+++ 4. Intermediate+/birthday-wisher-normal-start/main.py
@@ -11,6 +11,8 @@ PASSWORD = "abcd1234()"
 today_tuple = (datetime.now().month, datetime.now().day)
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+print(birthdays_dict)
+
 if today_tuple in birthdays_dict:
     birthdays_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
@@ -25,7 +27,6 @@ if today_tuple in birthdays_dict:
             from_addr=MY_EMAIL,
             to_addrs=birthdays_person["email"],
             msg=f"Subject:Happy Birthday\n\n{contents}")
-
 
 # 1. Update the birthdays.csv with your friends & family's details.
 # HINT: Make sure one of the entries matches today's date for testing purposes. e.g.
